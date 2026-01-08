07ce48a900e7d2434c6963307357e7ac2ad2fffb
xochilpili <xochilpili@gmail.com>
2026-01-07 19:51:33 -0600

    remove comment

--- internal/providers/manager.go
+++ internal/providers/manager.go
@@ -48,15 +48,6 @@ type Manager struct {
 	handlers map[string]Handler
 }
 
-/*
-SubX API (unknown documentation)
-https://subx-api.duckdns.org/api/subtitles/search?title=sinners+2025
-https://subx-api.duckdns.org/api/subtitles/9bb65e75-0d75-4d99-8c61-65680a023c23/download
-
-Authorization: Bearer 8PgmDkZg_8PgmDkZgscoCZtAwWI6oFNeAvbneYjdZTqhwBwT3lqk
-content-type: application/x-www-form-urlencoded
-content-disposition: attachment
-*/
 func New(config *config.Config, logger *zerolog.Logger) *Manager {
 	r := resty.New()
 	handlers := map[string]Handler{
